287cd9b45087b5b7fafabfa72906e371be741ef9
Philip osir Otieno <philiposir2@gmail.com>
2024-08-31 13:04:18 +0300

    updates

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    a, b = 0, 1
+
+    sequence = []
+
+
+    while len(sequence) < length:
+        sequence.append(a)
+        a, b = b, a + b
+
+    print(sequence)
+    # return sequence
+# print(print_fibonacci(9))
